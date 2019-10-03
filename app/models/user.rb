68ca932f5d39772bf5ef2dd9a8889911b4e4a9fa
Alexey S. Matveev <as.luden@gmail.com>
2019-10-04 00:28:01 +0400

    Validate email using RegExp

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
 
-  validates :email, presence: true, uniqueness: true
+  EMAIL_REGEX = /\A[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\z/i
+
+  validates :email, :presence => true, :uniqueness => true, :format => EMAIL_REGEX
 end
