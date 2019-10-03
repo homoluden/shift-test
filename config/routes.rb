a89f13962c9161dbd00adcfbf7a9c68c2134f3a9
Alexey S. Matveev <as.luden@gmail.com>
2019-10-04 00:09:19 +0400

    Implement SignUp and SignIn + Session store

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+  root 'home#index'
+
   # TODO: think about providing REST API for React components (maybe prefixed w/ 'api' term)
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'home#index'
 end
