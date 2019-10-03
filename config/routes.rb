d6fc1d46230d3ca41d547428a205661c6fd83fd9
Alexey S. Matveev <as.luden@gmail.com>
2019-10-03 23:44:37 +0400

    Add TODO about API w/ prefix

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  # TODO: think about providing REST API for React components (maybe prefixed w/ 'api' term)
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'home#index'
